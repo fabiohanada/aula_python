f9174850bba0be99fbbf018676f603d0bef64561
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2024-11-07 20:51:11 -0300

    filter 2

--- pythonProject/filter.py
+++ pythonProject/filter.py
@@ -1,9 +1,6 @@
 """
 filter
 
-
-"""
-
 import statistics
 
 dados = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
@@ -14,3 +11,22 @@ print(f'Media: {media}')
 
 res = filter(lambda valor: valor > media, dados)
 print(list(res))
+
+print(usuarios)
+
+"""
+
+usuarios = [
+    {"username": "samuel", "tweets": ["bolos", "pizzas"]},
+    {"username": "carla", "tweets": ["gatos"]},
+    {"username": "jeff", "tweets": []},
+    {"username": "bob", "tweets": []},
+    {"username": "dog", "tweets": ["cao"]},
+    {"username": "gal", "tweets": []}
+]
+
+
+
+inativos = list(filter(lambda usuario: len(usuario['tweets']) == 0, usuarios))
+
+print(inativos)
