031d84f576551c8cd454f2ca47f53dd2def900d2
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2024-11-07 06:25:56 -0300

    filter

--- pythonProject/filter.py
+++ pythonProject/filter.py
@@ -1,5 +1,16 @@
 """
-
+filter
 
 
 """
+
+import statistics
+
+dados = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
+
+media = statistics.mean(dados)
+
+print(f'Media: {media}')
+
+res = filter(lambda valor: valor > media, dados)
+print(list(res))
