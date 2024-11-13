6f51f78f6a4a26c9d002fd97c92bd126b9901bec
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2024-11-13 06:48:19 -0300

    erros

---
+++ pythonProject/erros_mais _comuns.py
@@ -0,0 +1,5 @@
+"""
+
+erros mais comuns
+
+"""
---
+++ pythonProject/reversed.py
@@ -0,0 +1,19 @@
+"""
+
+diferente do reserve
+"""
+
+lista = [1, 2, 3, 4, 5]
+
+res = reversed(lista)
+
+res = reversed(lista)
+
+print(res)
+
+print(type(res))
+
+print(list(reversed(lista)))
+
+print(list(lista))
+
