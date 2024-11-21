20cc9d1529da6ec008d48c9bd3a23396375f357d
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2024-11-21 06:46:08 -0300

    modulos builtin

---
+++ pythonProject/modulo_random.py
@@ -0,0 +1,14 @@
+"""
+
+from random import random
+
+for i in range(10):
+    print(random())
+
+
+"""
+
+from random import uniform
+
+for i in range(10):
+    print(uniform(3, 7))
---
+++ pythonProject/modulos_builtin.py
@@ -0,0 +1,38 @@
+"""
+
+modulos integrados
+
+import random as rdm
+
+print(rdm.random())
+
+import random *
+importa tudo
+
+from random import randint as rdi, random as rdm
+
+print(rdi(5,89))
+
+print(rdm())
+
+"""
+
+from random import (
+    random,
+    randint,
+    shuffle,
+    choice
+)
+
+print(random())
+
+print(randint(3, 7))
+
+lista = ['fabio', 'hanada', 'jogos']
+shuffle(lista)
+print(lista)
+
+print(choice('Fabio'))
+
+
+
