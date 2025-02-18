aae734a5bab58a18599f713f4cf1e844150c98fd
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2025-02-17 21:20:40 -0300

    bloco with

--- pythonProject/bloco_with.py
+++ pythonProject/bloco_with.py
@@ -1,2 +1,20 @@
-with open('texto.txt') as arquivo:
-    print(arquivo.readline())
+"""
+
+
+def soma(numero1, numero2):
+    valor_somado = numero1 + numero2
+    return valor_somado
+
+valor = soma(12, 12)
+
+print(valor)
+
+"""
+def f(n):
+    if n < 3:
+        return n - 1
+    else:
+        return f(n-2) + f(n-1)
+valor = f(10)
+
+print(valor)
--- pythonProject/escrever_arquivos.py
+++ pythonProject/escrever_arquivos.py
@@ -1,10 +1,30 @@
 """
 
-
-
-"""
-
 with open('texto.txt', 'w') as arquivo:
     arquivo.write('escre eh facil.\n')
     arquivo.write('podemos.\n')
     arquivo.write('ultima')
+
+
+arquivo = open('mais.txt', 'w')
+
+arquivo.write('Um texto qualquer.\n')
+arquivo.write('mais')
+
+arquivo.close()
+
+
+with open('geek.txt', 'w') as arquivo:
+    arquivo.write('Geek ' * 1000)
+"""
+with open('frutas.txt', 'w') as arquivo:
+    while True:
+        fruta = input('informe uma fruta: ')
+        if fruta != 'sair':
+            arquivo.write(fruta)
+            arquivo.write('\n')
+        else:
+            break
+
+
+
